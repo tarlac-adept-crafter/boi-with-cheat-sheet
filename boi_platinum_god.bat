353e17027d899bc5758ffd8fa08b0a099047291b
tarlac-adept-crafter <tarlacadeptcrafter@gmail.com>
2021-06-12 20:17:29 -0500

    Added config variables section and short description

--- boi_platinum_god.bat
+++ boi_platinum_god.bat
@@ -1,7 +1,19 @@
-@ECHO OFF
+@echo off
 
-REM Launches The Binding of Isaac
-start steam://rungameid/250900
+REM Use this script to launch The Binding of Isaac. It will also launch
+REM the Isaac Cheat Sheet from Platinum God in your default web browser.
 
-REM Opens the cheat sheet in the default web browser
+REM ---------- CONFIG VARIABLES ----------
+
+REM The Binding of Isaac Steam URI
+set "GAME_URI=steam://rungameid/250900"
+
+REM ---------- END CONFIG VARIABLES ----------
+
+REM Start the game
+echo Launching game URI: %GAME_URI%
+start %GAME_URI%
+
+REM Add other programs to launch
+echo Starting Isaac Cheat Sheet at http://platinumgod.co.uk/...
 start http://platinumgod.co.uk/
